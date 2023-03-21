5804618c16656fd270ebf64c5be98ba166d1c93d
LanceNelson <18687201+LanceNelson@users.noreply.github.com>
2023-03-21 17:09:56 -0500

    Initial Checkin

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById("merch").addEventListener("click", 
+    function() {
+        window.open("https://cats4nelson.myspreadshop.com");
+    })
+}
