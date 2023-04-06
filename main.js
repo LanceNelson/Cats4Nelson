5f1b265a5c680d56ef9026eb925bd80e408bd2d2
Lance <18687201+LanceNelson@users.noreply.github.com>
2023-04-05 19:54:43 -0500

    update Merch link

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     document.getElementById("merch").addEventListener("click", 
     function() {
-        window.open("https://cats4nelson.myspreadshop.com");
+        window.open("https://www.amazon.com/shop/louisenelson");
+        //window.open("https://cats4nelson.myspreadshop.com");
     })
 }
